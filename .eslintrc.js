aaeb4fb36f1454859a204d53eefe6c16d384ed52
Shoko Sugito <fruitmachine0@gmail.com>
2022-07-22 22:07:11 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
+  },
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unreachable': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-useless-return':
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'vue/order-in-components':
+      process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+  },
+}
