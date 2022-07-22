37003a946cf1947bb17232774b3e01280693c789
Shoko Sugito <fruitmachine0@gmail.com>
2022-07-22 22:31:46 +0900

    chore: firebase

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,12 +41,29 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/firebase',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
+  },
+
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyBGf6YNafw3BxPFwoshfyzuZzXaOdKgzlY',
+      authDomain: 'food-database-4a00f.firebaseapp.com',
+      projectId: 'food-database-4a00f',
+      storageBucket: 'food-database-4a00f.appspot.com',
+      messagingSenderId: '575453396956',
+      appId: '1:575453396956:web:4910a9a59556c1d1e1d919',
+      measurementId: 'G-E81GPFW453',
+    },
+    services: {
+      auth: true,
+      firestore: true,
+    },
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
