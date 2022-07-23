1fc6d6c7a5e2b3d753fedacb45acbe1933e7d0b7
Shoko Sugito <fruitmachine0@gmail.com>
2022-07-23 17:34:53 +0900

    chore: ESLint typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,20 @@ module.exports = {
     browser: true,
     node: true,
   },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
-  plugins: ['prettier'],
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  ignorePatterns: ['types/**/*.*'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
