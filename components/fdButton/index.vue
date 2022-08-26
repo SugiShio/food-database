e944fac75dabf0425c4fdd06f0164498b93fa868
Shoko Sugito <fruitmachine0@gmail.com>
2022-08-26 09:10:51 +0900

    feat: 押せないボタン

--- components/fdButton/index.vue
+++ components/fdButton/index.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 button.fd-button(
   :class='type',
+  :disabled='disabled',
   :type='typeAttribute',
   @click='$emit("button-clicked")'
 ) {{ label }}
@@ -11,6 +12,7 @@ import Vue from 'vue'
 export default Vue.extend({
   name: 'FdButton',
   props: {
+    disabled: { type: Boolean, default: false },
     label: { type: String, default: '' },
     typeAttribute: { type: String, default: 'button' },
     type: { type: String, default: '' },
@@ -29,7 +31,15 @@ export default Vue.extend({
   transition: background-color 0.3s;
 
   &:hover {
+    border-color: rgba($color-main, 0.7);
     background-color: rgba($color-main, 0.7);
+  }
+
+  &[disabled],
+  &[disabled]:hover {
+    background-color: rgba($color-main, 0.5);
+    border-color: rgba($color-main, 0.5);
+    cursor: default;
   }
 
   &.secondary {
@@ -38,6 +48,7 @@ export default Vue.extend({
 
     &:hover {
       background-color: rgba($color-main, 0.1);
+      border-color: rgba($color-main, 0.1);
     }
   }
 }
