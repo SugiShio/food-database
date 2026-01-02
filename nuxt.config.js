79b7bdb2ad6245aeaa48508ca7fc7a45d3af0e92
Shoko Sugito <fruitmachine0@gmail.com>
2026-01-02 20:08:21 +0900

    feat: ファビコン・ホーム画面アイコン追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,10 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/icon.png' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
